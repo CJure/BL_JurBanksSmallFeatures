c2910d3a32009d51e14278107351f36e8209111d
DESKTOP-7PLKRFC\jur_1 <jure.cerovsek@gmail.com>
2020-04-14 11:41:31 +0200

    Code cleaning, we check if game is campaign or something else before we load modules

--- JurBanksFeatures/MySubModule.cs
+++ JurBanksFeatures/MySubModule.cs
@@ -1,33 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.ViewModelCollection.Map;
 using TaleWorlds.Core;
-using TaleWorlds.Engine;
-using TaleWorlds.Engine.Screens;
-using TaleWorlds.InputSystem;
 using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
-using TaleWorlds.MountAndBlade.Source.Missions;
+
 
 namespace JurBanksFeatures
 {
     //Credits for helping: Australian, Zervox and Doombox
     public class MySubModule : MBSubModuleBase
     {
-        int troopCount = 0;
+        bool enableFollowFeature = true;
+        bool enableHideoutFeature = true;
+
         protected override void OnSubModuleLoad()
         {
-            
-            /*  Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
-              new TextObject("Message", null),
-              9990,
-              () => { InformationManager.DisplayMessage(new InformationMessage("Hello World!")); },
-              false));*/
+            var module = Module.CurrentModule;
+            module.AddInitialStateOption(new InitialStateOption(
+            "JurbankOptions",
+            new TextObject("JurBankFeatures options"),
+            10001,
+            ShowModOptions,
+            false
+          ));
+            base.OnSubModuleLoad();
+        }
+
+        private void ShowModOptions()
+        {
+            var elements = new List<InquiryElement>();
+
+            elements.Add(new InquiryElement(
+              nameof(enableFollowFeature),
+              enableFollowFeature ? "Disable follow party" : "Enable follow party",
+              null
+            ));
+
+            elements.Add(new InquiryElement(
+              nameof(enableHideoutFeature),
+              enableHideoutFeature ? "Disable hideout feature" : "Enable hideout feature",
+              null
+            ));
+           
+            InformationManager.ShowMultiSelectionInquiry(new MultiSelectionInquiryData(
+              "Mod Options",
+              "JurBankFeatures options",
+              elements,
+              true,
+              true,
+              "Apply",
+              "Return",
+              list => {
+                  var selected = (string)list[0].Identifier;
+                  switch (selected)
+                  {
+                      case nameof(enableFollowFeature):
+                          enableFollowFeature = !enableFollowFeature;
+                          ShowMessage($"Follow feature: {(enableFollowFeature ? "Enabled" : "Disabled")}.");
+                          //Options.Save();
+                          break;
+                      case nameof(enableHideoutFeature):
+                          enableHideoutFeature = !enableHideoutFeature;
+                          ShowMessage($"Hideout feature: {(enableHideoutFeature ? "Enabled" : "Disabled")}.");
+                          //Options.Save();
+                          break;
+                      default:
+                          throw new NotImplementedException(selected);
+                  }
+              }, null));
+        }
+
+        private static void ShowMessage(string msg)
+        {
+            InformationManager.DisplayMessage(new InformationMessage(msg));
         }
 
         private void getAllHideouts()
@@ -44,15 +91,27 @@ namespace JurBanksFeatures
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             base.OnGameStart(game, gameStarterObject);
-            AddModels(gameStarterObject as CampaignGameStarter);
+            if (gameStarterObject is CampaignGameStarter cgs)
+            {
+                AddModels(gameStarterObject as CampaignGameStarter);
+            }
         }
 
         private void AddModels(CampaignGameStarter gameStarter)
         {
-            gameStarter.AddBehavior(new FollowPartyBehaviour());
-            gameStarter.AddModel(new SetPlayerSpeed());
+            Debug.WriteLine("follow: " + enableFollowFeature + ", hideout: " + enableHideoutFeature);
+            if(enableFollowFeature)
+            {
+                gameStarter.AddBehavior(new FollowPartyBehaviour());
+                gameStarter.AddModel(new SetPlayerSpeed());
+            }
+            if(enableHideoutFeature)
+            {
+                gameStarter.AddBehavior(new HideoutMission());
+            }
+            
             //gameStarter.AddModel(new HideoutMission());
-            gameStarter.AddBehavior(new HideoutMission());
+            
             //    gameStarter.AddModel(new HideoutMission());
         }
 
